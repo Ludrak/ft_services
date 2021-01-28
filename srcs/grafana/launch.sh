c6290f63984766aaa81a594c6e1970e55a6db217
arthur6996 <arthuro6996@live.fr>
2021-01-28 14:23:11 +0100

    start of grafana image

---
+++ srcs/grafana/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+while true
+do
+sleep 1
+done
+
+
+#	CMD BUILD IMAGE ALPINE AND EXEC IT
+#
+#	if docker of minikube => eval $(minikube docker-env)
+#	docker build -t alpine_grafana .
+#
+#	docker run -it alpine_grafana sh
