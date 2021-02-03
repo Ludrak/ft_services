876808aa8dceeff0697d0062e822a077cddc82aa
Nathan <maximenathan@hotmail.fr>
2021-02-03 20:03:59 +0100

    wp plugins auto up

--- srcs/services/wordpress/config/wordpress/setupWp.sh
+++ srcs/services/wordpress/config/wordpress/setupWp.sh
@@ -3,13 +3,20 @@
 # start telegraf
 cd /telegraf-1.15.2/usr/bin/ && ./telegraf &
 
-#manage wordpress plug-in
-wp plugin install theme-my-login --activate --path=${WWW_ROOT}
-wp plugin uninstall akismet --deactivate --path=${WWW_ROOT}
-wp plugin uninstall hello --deactivate --path=${WWW_ROOT}
-
 # start nginx and php server
 php-fpm7
 
 nginx -t 2>&1 >> wp.log
-nginx
+
+nginx &
+
+sleep 30
+
+wp plugin install theme-my-login --activate --path=${WWW_ROOT}
+wp plugin uninstall akismet --deactivate --path=${WWW_ROOT}
+wp plugin uninstall hello --deactivate --path=${WWW_ROOT}
+
+while [ 1 ]
+do 
+    sleep 1
+done
