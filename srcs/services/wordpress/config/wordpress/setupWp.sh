5db53d1dadf125bb2de67f3f941c855cfac5889c
Nathan <maximenathan@hotmail.fr>
2021-02-03 11:37:20 +0100

    wp plugin add theme-my-login and delete akismet,hello

--- srcs/services/wordpress/config/wordpress/setupWp.sh
+++ srcs/services/wordpress/config/wordpress/setupWp.sh
@@ -3,11 +3,13 @@
 # start telegraf
 cd /telegraf-1.15.2/usr/bin/ && ./telegraf &
 
+#manage wordpress plug-in
+wp plugin install theme-my-login --activate --path=${WWW_ROOT}
+wp plugin uninstall akismet --deactivate --path=${WWW_ROOT}
+wp plugin uninstall hello --deactivate --path=${WWW_ROOT}
+
 # start nginx and php server
 php-fpm7
-
-#install wordpress plug-in
-wp plugin install theme-my-login --activate --path=${WWW_ROOT} 2>&1 >> wp.log
 
 nginx -t 2>&1 >> wp.log
 nginx
